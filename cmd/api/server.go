ed87c3633e76bcd01e8df3aa84c2792b57e4110f
tmgasek <tomaszgasek28@gmail.com>
2023-10-31 17:55:34 +0000

    graceful shutdown done

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+// When we receive a SIGINT or SIGTERM, instruct srv to stop accepting new HTTP
+// reqs, give any in flight HTTP reqs a grace period of 20 secs befor srv dies.
 
 func (app *application) serve() error {
 
@@ -18,10 +26,53 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	// Use this chan to receive any errors returned by graceful Shutdown()
+	shutdownError := make(chan error)
+
+	// Start a bg goroutine to listen for signals.
+	go func() {
+		// Make a buffered quit channel.
+		quit := make(chan os.Signal, 1)
+
+		// Listen for oncoming signals and relay them to quit channel.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This will block until a signal
+		// is received.
+		s := <-quit
+
+		app.logger.PrintInfo("shutting down server", map[string]string{
+			"signal": s.String(),
+		})
+
+		// New context with a 20 sec timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.PrintInfo("starting server", map[string]string{
 		"add": srv.Addr,
 		"env": app.config.env,
 	})
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	// If we get this err, indication that graceful shutdown has started - Good.
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	// Wait to receive return value from Shutdown() on shutdownError chan.
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	// At this point, graceful shutdown successful.
+	app.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
 }
